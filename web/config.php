6194e3c74fceb6003ac74cb57115093125d11a2e
Justin Roeder <92489099+delphijustin@users.noreply.github.com>
2022-11-20 16:04:47 -0600

    Create config.php

---
+++ web/config.php
@@ -0,0 +1,11 @@
+<?php
+$pin=0;//the secret pin# used to change channels.
+$udvrdir="/cygdrive/c/delphijustin/";
+$lastremo_bat=$udvrdir."lastremo.bat";//filename for where the channel commands are written to
+$xmltvenabled=false;//Set to true to use xmltv if you have the correct xml files
+$tvguide=$udvrdir."tvguide.xml";//path to tvguide.xml, this file is made by xmltvlistings.com
+$channels_xml=$udvrdir."channels.xml";
+$channels_db=$udvrdir."channels.db";//filename for channel database
+$channels_js=$udvrdir."channels.js";//for web-based tvguide
+$hlsurl="tv.m3u8";//url to hls m3u8 stream file
+?>
